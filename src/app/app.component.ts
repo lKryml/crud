5c163c1fd4355e6859be2f615218bc7b821aac67
Krym <gumimash@gmail.com>
2024-02-27 23:13:37 +0200

    crud test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'crud';
+  constructor(private dataService: DataService) {}
+  ngOnInit(): void {
+    this.dataService.getData().subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
---
+++ src/app/data.service.ts
@@ -0,0 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  constructor(private http: HttpClient) {}
+
+  getData() {
+    return this.http.get('http://195.234.122.131:8220/Controller/all_books');
+  }
+}
