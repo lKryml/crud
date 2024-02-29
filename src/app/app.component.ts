5bb60080a9d427eb59d16fbe09e9568b7b4ff9a5
Krym <gumimash@gmail.com>
2024-02-29 13:58:51 +0200

    post/get working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,19 @@ import { DataService } from './data.service';
 })
 export class AppComponent implements OnInit {
   title = 'crud';
+  queryParams = {
+    Name: 'aaaaaaa',
+    Quantity: 2,
+    Author: 'aaaaa',
+  };
   constructor(private dataService: DataService) {}
   ngOnInit(): void {
     this.dataService.getData().subscribe((data) => {
       console.log(data);
+    });
+
+    this.dataService.addBook(this.queryParams).subscribe((res) => {
+      console.log(res);
     });
   }
 }
--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -10,4 +10,12 @@ export class DataService {
   getData() {
     return this.http.get('http://195.234.122.131:8220/Controller/all_books');
   }
+
+  addBook(queryParams: any) {
+    return this.http.post(
+      'http://195.234.122.131:8220/Controller/add_book',
+      null,
+      { params: queryParams }
+    );
+  }
 }
